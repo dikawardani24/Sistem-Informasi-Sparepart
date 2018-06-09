486ae976ad4f53148e59fdd91bc6dfa62880490c
dikawardani24 <dikawardani24@gmail.com>
2018-06-09 19:16:48 +0700

    Change JPA Starter libs

---
+++ src/com/reckitBekinser/activity/main/SparepartKeluarController.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018 dika.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    dika - initial API and implementation and/or initial documentation
+ */
+package com.reckitBekinser.activity.main;
+
+/**
+ *
+ * @author dika
+ */
+public class SparepartKeluarController {
+    
+}
